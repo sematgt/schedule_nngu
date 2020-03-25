e05a3d5d2dc9b97507945b5e33c14109278b6919
sgbliznyuk <sgbliznyuk@gmail.com>
2020-03-25 08:32:25 +0300

    fixed some styles

--- frontend/src/Components/Body/ScheduleCellWithWeekDays.js
+++ frontend/src/Components/Body/ScheduleCellWithWeekDays.js
@@ -8,18 +8,18 @@ export default function ScheduleCellWithWeekDays(props) {
 
     return( 
             <div 
-                className={["Schedule-cell", (props.day.date === date.toISOString().slice(0,10) && props.study_mode === 'distance' || props.day.date === date.toISOString().slice(0,10) && current_week_parity === props.selected_week_fulltime) ? "Schedule-cell-weekday-today" : "Schedule-cell-weekday"].join(' ')} 
+                className={["Schedule-cell", ((props.day.date === date.toISOString().slice(0,10) && props.study_mode === 'distance') || (props.day.date === date.toISOString().slice(0,10) && current_week_parity === props.selected_week_fulltime)) ? "Schedule-cell-weekday-today" : "Schedule-cell-weekday"].join(' ')} 
                 key={props.day.day}
             >
             <div className={props.study_mode === 'distance' ? "wday" : "wday_big"}>
-                <div>
+                <div className={props.study_mode === 'distance' ? "" : "wday_big_text"}>
                     {props.day.wday}
                 </div>
             </div>
             {
                 props.study_mode === 'distance' &&
                     <div className="day">
-                        <div>
+                        <div className="day_text">
                             {props.day.day}
                         </div>
                     </div>
