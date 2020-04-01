6d99fbf10f1d80368c424f7727c322c03722526e
sgbliznyuk <sgbliznyuk@gmail.com>
2020-04-01 20:17:57 +0300

    added loading indicator and lessons api fetching

--- frontend/src/Admin/Admin.js
+++ frontend/src/Admin/Admin.js
@@ -8,58 +8,8 @@ import { ApiURI } from '../AppConfig';
 
 export default function Admin() {
     
-    // State hooks
-    
-    const [groupValue, setGroupValue] = React.useState('');
-    const [groupChoices, setGroupChoices] = React.useState([]);
-    const handleChangeGroup = (event, value) => {
-        value ? setGroupValue(value.id) : setGroupValue('');
-    }
+    // Constants
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await fetch(ApiURI + '/groups/')
-            .then(response => response.json());
-            
-            setGroupChoices(result);
-        };
-        fetchData();
-    },[])
-    
-    
-    const [termValue, setTermValue] = React.useState('');
-    const [termChoices, setTermChoices] = React.useState([]);
-    const handleChangeTerm = (event) => {
-        setTermValue(event.target.value);
-    };
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await fetch(ApiURI + '/terms/')
-            .then(response => response.json());
-            
-            setTermChoices(result);
-        };
-        fetchData();
-    },[])
-
-    const [loads, setLoads] = React.useState([]);
-
-    useEffect(() => {
-        if (termValue && groupValue) {
-            const fetchData = async () => {
-                const result = await fetch(ApiURI + '/loads/')
-                .then(response => response.json())
-                .then(result => {
-                    setLoads(
-                        result.filter(load => load.group == groupValue && load.term == termValue)
-                    )
-                });
-            };
-            fetchData();
-        }
-    },[groupValue, termValue])
-    
     const loadTitles = [
         'Предмет', 
         'Количество часов', 
@@ -68,6 +18,104 @@ export default function Admin() {
         'Пар в неделю'
     ]
 
+    // Tables styles
+
+    const useStyles = makeStyles({
+        table: {
+            maxWidth: 950,
+        },
+        root: {
+            maxWidth: 950,
+        },
+    });
+
+    // State hooks
+    
+    const [choosenGroup, setChoosenGroup] = React.useState('');
+    const [groupChoices, setGroupChoices] = React.useState([]);
+    
+    const [choosenTerm, setChoosenTerm] = React.useState('');
+    const [termChoices, setTermChoices] = React.useState([]);
+    
+    const [loads, setLoads] = React.useState([]);
+
+    const [lessons, setLessons] = React.useState([]);
+
+    // handle components changes
+
+    const handleChangeTerm = (event) => {
+        setChoosenTerm(termChoices.filter(term => term.number === event.target.value)[0]);
+    };
+
+    const handleChangeGroup = (event, value) => {
+        value ? setChoosenGroup(value) : setChoosenGroup('');
+    }
+
+    // data fetching
+
+    const [groupsLoading, setGroupsLoading] = React.useState(false);
+    const [termsLoading, setTermsLoading] = React.useState(false);
+    const [groupsError, setGroupsError] = React.useState(false);
+    const [termsError, setTermsError] = React.useState(false);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setGroupsLoading(true);            
+            const result_groups = await fetch(ApiURI + '/groups/')
+            .then(response => response.json());
+            setGroupChoices(result_groups);
+            setGroupsLoading(false);
+        };
+        fetchData();
+    },[])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setTermsLoading(true); 
+            const result_terms = await fetch(ApiURI + '/terms/')
+            .then(response => response.json());
+            setTermChoices(result_terms);
+            setTermsLoading(false); 
+        };
+        fetchData();
+    },[])
+  
+    useEffect(() => {
+        if (choosenTerm && choosenGroup) {
+            const fetchData = async () => {
+                const result = await fetch(ApiURI + '/loads/')
+                .then(response => response.json())
+                .then(result => {
+                    setLoads(
+                        result.filter(load => load.group === choosenGroup.id && load.term === choosenTerm.id)
+                    )
+                });
+            };
+            fetchData();
+        };
+
+        if (choosenGroup.mode_of_study === 'distance') {
+            const fetchData = async () => {
+                const result = await fetch(ApiURI + '/lessons_distance/')
+                .then(response => response.json());
+
+                setLessons(result);
+                };
+                fetchData();
+            };
+            
+        if (choosenGroup.mode_of_study === 'fulltime') {
+            const fetchData = async () => {
+                const result = await fetch(ApiURI + '/lessons_fulltime/')
+                .then(response => response.json());
+
+                setLessons(result);
+                };
+                fetchData();
+            };
+
+    },[choosenGroup, choosenTerm])
+    
     
     function createData(name, calories, fat, carbs, protein) {
         return { name, calories, fat, carbs, protein };
@@ -81,52 +129,42 @@ export default function Admin() {
         createData('Gingerbread', 356, 16.0, 49, 3.9),
     ];
     
-    // Tables styles
 
-    const useStyles = makeStyles({
-        table: {
-            maxWidth: 950,
-        },
-        root: {
-            maxWidth: 950,
-        },
-    });
 
     return (
         <div>
             <h2>
             Admin interface
             </h2>
-            <ComboBox 
-                label="Группа"
-                options={groupChoices}
-                clearText="Очистить"
-                noOptionsText="Группа не найдена"
-                autoHighlight={true}
-                handleChange={handleChangeGroup}
-                
-            />
-            <span>
-                {groupValue}
-            </span>
+            {console.log(groupsLoading, termsLoading)}
+            {
+                groupsLoading ? (<div><i>Groups is loading...</i></div>) : (
+                    <ComboBox 
+                        label="Группа"
+                        options={groupChoices}
+                        clearText="Очистить"
+                        noOptionsText="Группа не найдена"
+                        autoHighlight={true}
+                        handleChange={handleChangeGroup}
+                    />
+                )
+            }
             <br />
-            <RadioButtonsGroup 
-                label="Term"
-                handleChange={handleChangeTerm}
-                choices={termChoices}
-            />
-            <span>
-                {termValue}
-            </span>
+            {
+                termsLoading ? (<div><i>Terms is loading...</i></div>) : (
+                    <RadioButtonsGroup 
+                        label="Term"
+                        handleChange={handleChangeTerm}
+                        choices={termChoices}
+                    />
+                )
+            }
             <br />
             <SimpleTable
                 titles={loadTitles}
                 useStyles={useStyles}
                 rows={rows}
              />
-             <div>
-                 {console.log({loads})}
-             </div>
         </div>
     )
 }
